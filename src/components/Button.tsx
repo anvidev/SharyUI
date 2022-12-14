ec7eca24aeef180a8b9c79e60a789df382fb7bed
Andreas Vifert <andreasgylche@gmail.com>
2022-12-14 13:05:35 +0100

    added example component

---
+++ src/components/Button.tsx
@@ -0,0 +1,34 @@
+import React, { HTMLAttributes, ReactNode } from 'react';
+
+export interface Props extends HTMLAttributes<HTMLButtonElement> {
+  /** Provide a text for the button */
+  children: ReactNode;
+  /** Which variant look would you like to use */
+  variant: 'primary' | 'secondary';
+}
+
+/**  This is a special button!!! */
+export const Button = ({ children, variant = 'primary', ...props }: Props) => {
+  return (
+    <button
+      {...props}
+      style={{
+        backgroundColor: variant === 'primary' ? '#5b2ee2' : '#078c54',
+        minWidth: '175px',
+        height: '45px',
+        margin: '10px auto',
+        border: 'none',
+        borderRadius: '4px',
+        color: '#fff',
+        fontStyle: 'normal',
+        fontWeight: '700',
+        fontSize: '16px',
+        lineHeight: '24px',
+        transition: '0.3s ease',
+        cursor: 'pointer',
+      }}
+    >
+      {children}
+    </button>
+  );
+};
