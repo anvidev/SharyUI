65db27cefc4b2f0f753f57e62739522d658dadfa
Andreas Vifert <andreasgylche@gmail.com>
2023-01-03 10:28:09 +0100

    updated library to compile tailwind classes to production

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,6 @@ export { Avatar } from './components/Avatar';
 export { Heading } from './components/Heading';
 export { Text } from './components/Text';
 export { Button } from './components/Button';
-export { ButtonExample } from './components/ButtonExample';
+export { Checkbox } from './components/Checkbox';
+export { Input } from './components/Input';
+export { Label } from './components/Label';
